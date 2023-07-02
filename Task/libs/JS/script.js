78a011de7e6a254c4da4b4e0ea9f25c6560ef504
Daniel Williams <danielkwasi@hotmail.co.uk>
2023-07-02 15:34:53 +0100

    added json function

--- Task/libs/JS/script.js
+++ Task/libs/JS/script.js
@@ -1,1 +1,43 @@
-console.log('hello');
+$(document).ready(function () {
+    $('#postcodebtn').click(function() {
+
+        console.log ($('#postcodeText').val());
+        console.log ($('#selCountry').val());
+        $.ajax({
+            url: "libs/php/getPostCode.php",
+            type:'POST',
+            dataType:'json',
+            data: {
+                country: $('#selCountry').val(),
+                postcode: $('#postcodeText').val(),
+            },
+            success: function(data) {
+                console.log('success');
+            },
+            error: function(data) {
+                console.log(data);
+            }
+        })
+        
+
+
+
+        
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
--- Task/libs/PHP/getPostCode.php
+++ Task/libs/PHP/getPostCode.php
@@ -1,9 +1,40 @@
-<?php 
+<?php
+// Logs message to a custom log file
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+
+error_log(print_r($_REQUEST['postcode'], true));
+error_log(print_r($_REQUEST['country'], true));
+
+$my_arr = array(1,2,3);
+   echo '<script>';
+   echo 'console.log('. json_encode($my_arr, JSON_HEX_TAG) .')';
+   echo '</script>';
 
 
 
 
+$executionStartTime = microtime(true);
 
+$url = `http://api.geonames.org/postalCodeLookupJSON?postalcode={$_REQUEST['postcode']}&country={$_REQUEST['country']}&username=kwasimodo`;
+
+
+
+$ch = curl_init();
+
+
+//Console.log(function)
+function console_log($output, $with_script_tags = true) {
+    $js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) . 
+');';
+    if ($with_script_tags) {
+        $js_code = '<script>' . $js_code . '</script>';
+    }
+    echo $js_code;
+}
+console_log($url)
 
 
 

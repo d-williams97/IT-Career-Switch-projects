aceddd36d19280f1e7e42080015f9e1fb65f5571
Daniel Williams <danielkwasi@hotmail.co.uk>
2023-07-03 12:05:15 +0100

    Get postcode API completed

--- Task/libs/JS/script.js
+++ Task/libs/JS/script.js
@@ -1,38 +1,52 @@
 $(document).ready(function () {
-    $('#postcodebtn').click(function() {
+  $("#postcodebtn").click(function () {
+    console.log($("#postcodeText").val());
+    console.log($("#selCountry").val());
+    $.ajax({
+      url: "libs/php/getPostCode.php",
+      type: "POST",
+      dataType: "json",
+      data: {
+        country: $("#selCountry").val(),
+        postcode: $("#postcodeText").val(),
+      },
+      success: function (data) {
+        console.log("successsssss");
+        const results = data.data.postalcodes;
 
-        console.log ($('#postcodeText').val());
-        console.log ($('#selCountry').val());
-        $.ajax({
-            url: "libs/php/getPostCode.php",
-            type:'POST',
-            dataType:'json',
-            data: {
-                country: $('#selCountry').val(),
-                postcode: $('#postcodeText').val(),
-            },
-            success: function(data) {
-                console.log('success');
-            },
-            error: function(data) {
-                console.log(data);
-            }
-        })
-        
+        if (results.length === 0) {
+          console.log(results);
+          console.log("postcode not found");
+        } else {
+          console.log(results);
+          for (const result of results) {
+            $("#countryCode").html(result.countryCode);
+            $("#city").html(result.placeName);
+            $("#country").html(result.adminName2);
+            $("#lat").html(result.lat);
+            $("#lng").html(result.lng);
+          }
+        }
 
-
-
-        
+        $("#postcodeText").val("");
+        $("#selCountry").val("");
+      },
+      error: function (data) {
+        console.log("err");
+        console.log(data);
+      },
     });
+  });
 
 
+// next function call 
+$("#wikibtn").click(function () {
+    console.log('wiki');
+    console.log($('#cityInput').val());
 
+    
 
-
-
-
-
-
+});
 
 
 
--- Task/libs/PHP/getPostCode.php
+++ Task/libs/PHP/getPostCode.php
@@ -5,36 +5,64 @@ ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
 
-error_log(print_r($_REQUEST['postcode'], true));
-error_log(print_r($_REQUEST['country'], true));
+$postalcode = $_REQUEST['postcode'];
+$postalcode = str_replace(' ','',$postalcode);
+$country = $_REQUEST['country'];
 
-$my_arr = array(1,2,3);
-   echo '<script>';
-   echo 'console.log('. json_encode($my_arr, JSON_HEX_TAG) .')';
-   echo '</script>';
-
-
+error_log(print_r($postalcode, true));
+error_log(print_r($country, true));
 
 
 $executionStartTime = microtime(true);
 
-$url = `http://api.geonames.org/postalCodeLookupJSON?postalcode={$_REQUEST['postcode']}&country={$_REQUEST['country']}&username=kwasimodo`;
+$url = 'http://api.geonames.org/postalCodeLookupJSON?postalcode=' . $postalcode . '&country=' . $country . '&username=kwasimodo';
 
+// function console_log($output, $with_script_tags = true) {
+//     $js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) . 
+// ');';
+//     if ($with_script_tags) {
+//         $js_code = '<script>' . $js_code . '</script>';
+//     }
+//     echo $js_code;
+// }
+
+// console_log($url);
 
 
 $ch = curl_init();
 
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+curl_setopt($ch, CURLOPT_URL, $url);
 
-//Console.log(function)
-function console_log($output, $with_script_tags = true) {
-    $js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) . 
-');';
-    if ($with_script_tags) {
-        $js_code = '<script>' . $js_code . '</script>';
-    }
-    echo $js_code;
-}
-console_log($url)
+
+$result = curl_exec($ch); 
+
+
+curl_close($ch);
+
+
+$decode = json_decode($result, true);
+
+
+$output['status']['code'] = "200";
+$output['status']['name'] = "ok";
+$output['status']['description'] = "success";
+$output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms"; //intval — Get the integer value of a variable
+$output['data'] = $decode;
+
+header('Content-Type: application/json; charset=UTF-8'); // header() is used to send a raw HTTP header which is stored in the response sent back with the data with echo below.
+
+echo json_encode($output); 
+
+
+// error_log(print_r($decode), true);
+
+
+
+
+
+
 
 
 

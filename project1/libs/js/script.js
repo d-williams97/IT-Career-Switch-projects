3c06c49efb55d24dbd8ede48703ae9332cef9684
Daniel Williams <danielkwasi@hotmail.co.uk>
2023-07-19 11:20:38 +0100

    Added country layer

--- project1/libs/js/script.js
+++ project1/libs/js/script.js
@@ -11,12 +11,12 @@ $(window).on("load", function () {
 });
 
 $(document).ready(function () {
+  let countryData;
   if (navigator.geolocation) {
     function showPosition(position) {
       let defaultLat = position.coords.latitude;
       let defaultLong = position.coords.longitude;
-
-          // ------------------------- INITIALIZE MAP -------------------------- //
+      // ------------------------- INITIALIZE MAP -------------------------- //
       let tileLayer1 = L.tileLayer(
         "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
         {
@@ -48,99 +48,137 @@ $(document).ready(function () {
 
     navigator.geolocation.getCurrentPosition(showPosition, geoError);
 
-
     // ------------------------- FETCHING JSON DATA -----------------------//
-
-    fetch('http://localhost/ITCS/project1/libs/assets/countryBorders.geo.json')
-    .then(response => response.json())
-    .then(data => {
+    fetch("http://localhost/ITCS/project1/libs/assets/countryBorders.geo.json")
+      .then((response) => response.json())
+      .then((data) => {
         const countryArr = data.features;
-        let countries = $.map(countryArr, function(country, i) {
-            let countryData = {
-                countryName: country.properties.name,
-                ISO: country.properties.iso_a2,
-                polygon: country.geometry.coordinates
-            }
-            return countryData
-        })
-        console.log(countries);
-        countries.sort(function(a,b) {
-            let countryA = a.countryName;
-            let countryB = b.countryName;
-            return countryA.localeCompare(countryB);
-        })
+        let countries = $.map(countryArr, function (country, i) {
+          countryData = {
+            countryName: country.properties.name,
+            ISO: country.properties.iso_a2,
+            polygon: country.geometry.coordinates,
+          };
+          return countryData;
+        });
+        // console.log(countries);
+        countries.sort(function (a, b) {
+          let countryA = a.countryName;
+          let countryB = b.countryName;
+          return countryA.localeCompare(countryB);
+        });
 
-            // ------------------------- CREATING COUNTRY HTML LIST ----------------------- //
+        // ------------------------- CREATING COUNTRY HTML LIST ----------------------- //
 
-        countryNames = $.map(countries, function(country, i) {
-            countryNames = country.countryName;
-            countryCodes = country.ISO
-            $('#selectCountry').append(`<option value='${countryCodes}'>${countryNames}</option>`)
-        });    
-    })
-    .catch(error => {
+        countryNames = $.map(countries, function (country, i) {
+          countryNames = country.countryName;
+          countryCodes = country.ISO;
+          $("#selectCountry").append(
+            `<option value='${countryCodes}'>${countryNames}</option>`
+          );
+        });
+      })
+      .catch((error) => {
         console.log(`Error: ${error}`);
         alert(`Error: ${error}`);
-     
-    });
-
+      });
   } else {
     console.log("Geolocation is not supported by this browser.");
   }
 
-
   //// ------------------------- COUNTRY LIST CHANGE EVENT ----------------------- /////////
 
-  $('#selectCountry').on('change', function() {
-    let selectedCountryOption = $(this).val();
-    console.log(selectedCountryOption);
+  $("#selectCountry").on("change", function () {
+    let selectedCountryCode = $(this).val();
+    let selectedCountryName = $("#selectCountry :selected").text();
+    console.log(selectedCountryCode, selectedCountryName);
 
-    //--------- AJAX CALL TO RETRIEVE LAT AND LOG DATA ------------------//
+    //--------- AJAX CALL TO RETRIEVE LAT,LNG,BOUNDS DATA ------------------//
     $.ajax({
-        url: 'libs/php/getOpenCageData.php',
-        type: 'POST',
-        dataType: 'json',
-        data: {
-            countryName: selectedCountryOption,
-        },
-        success: function (result) {
-            if (result.status.name == 'ok') {
-                console.log(result)
-            const geoNameCountryData = result.data.results[0];
-            console.log(geoNameCountryData);
-            let geoNameLat = geoNameCountryData.geometry.lat;
-            let geoNameLng = geoNameCountryData.geometry.lng;
-            console.log(geoNameLat,geoNameLng);
+      url: "libs/php/getOpenCageData.php",
+      type: "POST",
+      dataType: "json",
+      data: {
+        countryName: selectedCountryName,
+        countryCode: selectedCountryCode,
+      },
+      success: function (result) {
+        if (result.status.name == "ok") {
+          console.log(result);
 
-            //---   CHANGE MAP VIEW -------//  
-            map.setView([geoNameLat, geoNameLng], 10);
+          // ---- LAT,LNG DATA ----- //
+          const geoNameCountryData = result.data.results[0];
+          let geoNameLat = geoNameCountryData.geometry.lat;
+          let geoNameLng = geoNameCountryData.geometry.lng;
 
+          // ----- BOUNDS DATA ------ //
+          const countryBoundsObj = result.data.results[0].bounds;
+          const countryBoundsData = Object.values(countryBoundsObj);
+          console.log(countryBoundsData);
 
+          //--- CHANGE MAP VIEW -------//
+          map.fitBounds(countryBoundsData);
 
-            }
+          // --- ADD COUNTRY LAYER  -------//
+          fetch(
+            "http://localhost/ITCS/project1/libs/assets/countryBorders.geo.json"
+          )
+            .then((response) => response.json())
+            .then((data) => {
+              let arr = data.features;
+              let obj = $.grep(arr, function (e) {
+                return e.properties.name === selectedCountryName;
+              });
+              console.log(obj);
+              let countryCoordinates = obj[0].geometry.coordinates[0];
 
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-            console.log(textStatus);
-            console.log(errorThrown);
-          },
-    })
+              let latLng = [];
+              for (const coords of countryCoordinates) {
+                console.log(coords) //goes through all coords
+                let lat = coords[1]; // Latitude (y-coordinate) is the second element in the array
+                let lng = coords[0]; // Longitude (x-coordinate) is the first element in the array.
+                let point = L.latLng(lat, lng);
+                console.log(point)
+                latLng.push(point);
+              }
+              console.log(countryCoordinates);
+              console.log(latLng);
 
+              L.polygon(latLng)
+                .setStyle({
+                    color: "#ff7800",
+                    weight: 5,
+                    opacity: 0.65,
+                  })
+                .addTo(map);
 
+              // let myLines = [
+              //   {
+              //     type: "LineString",
+              //     coordinates: latLng // or countryCoordinates//
+              //   },
+              // ];
 
+              // var myStyle = {
+              //   color: "#ff7800",
+              //   weight: 5,
+              //   opacity: 0.65,
+              // };
 
-  })
+              // L.geoJSON(myLines, {
+              //   style: myStyle,
+              // }).addTo(map);
+            });
+        }
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+        console.log(textStatus);
+        console.log(errorThrown);
+      },
+    });
 
-
-
-
-
-
-
-
-
-
-
+    // FINDING ALTERNATIVE COORDINATES//
+  });
 
   // Map over JSON data for selects //
   //Store data for selected country//
--- project1/libs/php/getOpenCageData.php
+++ project1/libs/php/getOpenCageData.php
@@ -3,7 +3,15 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-$countryName = $_REQUEST['countryName'];
+$countryData = $_REQUEST['countryName'];
+$countryData2 = str_replace('Dem.','Democratic',$countryData);
+$countryData3 = str_replace('Lao PDR', 'Lao', $countryData2);
+$countryData4 = str_replace('W. Sahara', 'Western Sahara', $countryData3);
+$countryData5 = str_replace('Rep.', 'Republic', $countryData4);
+$countryData6 = str_replace("Côte d'Ivoire",'ci', $countryData5);
+$countryData7 = str_replace("Bosnia and Herz",'ba', $countryData6);
+$countryName = str_replace(' ', '%20', $countryData7);
+// $countryCode = $_REQUEST['countryCode'];
 error_log(print_r($countryName, true));
 
 $executionStartTime = microtime(true);
@@ -11,6 +19,7 @@ $executionStartTime = microtime(true);
 // key = e3489716a6574283b20a91a3349be943;
 // $url = 'https://api.opencagedata.com/geocode/v1/json?q=Germany&key=e3489716a6574283b20a91a3349be943&pretty=1';
 
+// $url = 'https://api.opencagedata.com/geocode/v1/json?q=' . $countryCode . ',' . $countryName . '&key=e3489716a6574283b20a91a3349be943&pretty=1';
 $url = 'https://api.opencagedata.com/geocode/v1/json?q=' . $countryName . '&key=e3489716a6574283b20a91a3349be943&pretty=1';
 
 $ch = curl_init();

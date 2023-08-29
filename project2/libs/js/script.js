34741b45d6dbe57ea1567881b20f4c54aa932a10
Daniel Williams <danielkwasi@hotmail.co.uk>
2023-08-29 15:51:30 +0100

    Search feature added

--- project2/libs/js/script.js
+++ project2/libs/js/script.js
@@ -11,92 +11,221 @@ $(window).on("load", function () {
 $(document).ready(function () {
   console.log("ready");
 
-  // LOADING EMPLOYEE TABLE //
+  let allEmployeeData;
 
+  let fillTable = (data) => {
+    for (let i = 0; i < data.length; i++) {
+      // console.log(allEmployeeData[i]);
+      let row = $(`<tr id=employeeRow'${[i]}'>`).addClass(
+        "d-flex align-items-center"
+      );
+
+      let name_JobTableCell = $("<td>")
+        .addClass("ps-4 cell-width")
+        .attr("data-id", i);
+      let namesDiv = $("<div>").addClass(
+        "d-flex flex-column align-items-start"
+      );
+      let namesData = $("<p>")
+        .html(`${data[i].firstName}, ${data[i].lastName}`)
+        .addClass("fw-bold text-start mb-0 employeeName");
+      let jobs = $("<p>").addClass("text-muted text-start mb-0");
+      let jobsData = $("<small>").html(data[i].jobTitle);
+
+      jobs.append(jobsData);
+      namesData.append(jobs);
+      namesDiv.append(namesData);
+      name_JobTableCell.append(namesDiv);
+      row.append(name_JobTableCell);
+
+      let emails = $("<td>")
+        .addClass("cell-width employeeEmail")
+        .attr("data-id", i);
+      emails
+        .html(allEmployeeData[i].email)
+        .addClass("text-nowrap align-middle d-md-table-cell ps-3");
+      row.append(emails);
+
+      let department_locationCell = $("<td>")
+        .addClass("cell-width pb-0 employeeDep")
+        .attr("data-id", i);
+      let departmentsDiv = $("<div>").addClass(
+        "d-flex flex-column align-items-start employeeDep"
+      );
+      let departmentsData = $("<p>")
+        .html(allEmployeeData[i].department)
+        .addClass("fw-bold text-start mb-0");
+      let locations = $("<p>").addClass("text-muted text-start mb-0");
+      let locationsData = $("<small>").html(data[i].location);
+
+      locations.append(locationsData);
+      departmentsData.append(locations);
+      departmentsDiv.append(departmentsData);
+      department_locationCell.append(departmentsDiv);
+      row.append(department_locationCell);
+
+      // -- BUTTONS -- //
+      let buttonCells = $("<td>").addClass("employeeCells");
+      let buttonDiv = $("<div>").addClass(
+        "d-flex justify-content-end employeeButtons me-3"
+      );
+      let editButtons = $("<button>")
+        .addClass("btn btn-none btn-sm")
+        .attr("data-bs-toggle", "modal")
+        .attr("data-bs-target", "#editEmployeeModal");
+      let editIcons = $("<i>").addClass(
+        "fa-solid fa-pencil fa-lg text-primary"
+      );
+      let deleteButtons = $("<button>")
+        .addClass("btn btn-none btn-sm ms-3")
+        .attr("data-bs-toggle", "modal")
+        .attr("data-bs-target", "#deleteEmployeeModal");
+      let deleteIcons = $("<i>").addClass(
+        "fa-regular fa-trash-can fa-lg text-danger"
+      );
+
+      editButtons.append(editIcons);
+      deleteButtons.append(deleteIcons);
+      buttonDiv.append(editButtons);
+      buttonDiv.append(deleteButtons);
+      buttonCells.append(buttonDiv);
+      row.append(buttonCells);
+
+      $("#employeeTableBody").append(row);
+    }
+  };
+
+  let fillDepartmentTable = (data) => {
+    for (let i = 0; i < data.length; i++) {
+      let department = data[i].department;
+      departments.push(department);
+
+      let row = $(`<tr id=departmentRow'${[i]}'>`).addClass("d-flex");
+
+      // -- DEPARTMENT COLUMN -- //
+      let departmentDepartmentCells = $("<td>")
+        .addClass("ps-4 departmentDepartmentCells")
+        .attr("data-id", i);
+      let departmentDepartmentDiv = $("<div>").addClass(
+        "d-flex align-items-start"
+      );
+      let departmentDepartmentNames = $("<p>")
+        .html(department)
+        .addClass("fw-bold text-start mb-0 departmentName");
+
+      departmentDepartmentDiv.append(departmentDepartmentNames);
+      departmentDepartmentCells.append(departmentDepartmentDiv);
+      row.append(departmentDepartmentCells);
+
+      // -- LOCATION COLUMN -- //
+      let departmentLocationCells = $("<td>")
+        .addClass("ps-4 departmentLocationCells")
+        .attr("data-id", i);
+      let departmentLocationDiv = $("<div>").addClass(
+        "d-flex align-items-start"
+      );
+      let departmentLocationNames = $("<p>")
+        .html(data[i].location)
+        .addClass("fw-bold text-start mb-0");
+
+      departmentLocationDiv.append(departmentLocationNames);
+      departmentLocationCells.append(departmentLocationDiv);
+      row.append(departmentLocationCells);
+
+      // -- BUTTONS -- //
+      let buttonCells = $("<td>").addClass("departmentButtonCells pb-4 pt-2");
+      let buttonDiv = $("<div>").addClass(
+        "d-flex justify-content-end employeeButtons me-3"
+      );
+      let editButtons = $("<button>")
+        .addClass("btn btn-none btn-sm")
+        .attr("data-bs-toggle", "modal")
+        .attr("data-bs-target", "#editDepartmentModal");
+      let editIcons = $("<i>").addClass(
+        "fa-solid fa-pencil fa-lg text-primary"
+      );
+      let deleteButtons = $("<button>")
+        .addClass("btn btn-none btn-sm ms-3")
+        .attr("data-bs-toggle", "modal")
+        .attr("data-bs-target", "#deleteDepartmentModal");
+      let deleteIcons = $("<i>").addClass(
+        "fa-regular fa-trash-can fa-lg text-danger"
+      );
+
+      editButtons.append(editIcons);
+      deleteButtons.append(deleteIcons);
+      buttonDiv.append(editButtons);
+      buttonDiv.append(deleteButtons);
+      buttonCells.append(buttonDiv);
+      row.append(buttonCells);
+
+      $("#departmentTableBody").append(row);
+    }
+    console.log(departments);
+  };
+
+  let fillLocationTable = (data) => {
+    for (let i = 0; i < data.length; i++) {
+      let location = data[i].location;
+      locations.push(location);
+
+      let row = $(`<tr id=locationRow'${[i]}'>`).addClass(
+        "d-flex align-items-center"
+      );
+
+      // -- LOCATION COLUMN -- //
+      let locationLocationCells = $("<td>")
+        .addClass("ps-4 locationLocationCells")
+        .attr("data-id", i);
+      let locationLocationDiv = $("<div>").addClass("d-flex align-items-start");
+      let locationLocationNames = $("<p>")
+        .html(location)
+        .addClass("fw-bold text-start mb-0");
+
+      locationLocationDiv.append(locationLocationNames);
+      locationLocationCells.append(locationLocationDiv);
+      row.append(locationLocationCells);
+
+      // -- BUTTONS -- //
+      let buttonCells = $("<td>").addClass("locationButtonCells pb-4 pt-2");
+      let buttonDiv = $("<div>").addClass(
+        "d-flex justify-content-end employeeButtons me-3"
+      );
+      let editButtons = $("<button>")
+        .addClass("btn btn-none btn-sm")
+        .attr("data-bs-toggle", "modal")
+        .attr("data-bs-target", "#editDepartmentModal");
+      let editIcons = $("<i>").addClass(
+        "fa-solid fa-pencil fa-lg text-primary"
+      );
+      let deleteButtons = $("<button>")
+        .addClass("btn btn-none btn-sm ms-3")
+        .attr("data-bs-toggle", "modal")
+        .attr("data-bs-target", "#deleteDepartmentModal");
+      let deleteIcons = $("<i>").addClass(
+        "fa-regular fa-trash-can fa-lg text-danger"
+      );
+
+      editButtons.append(editIcons);
+      deleteButtons.append(deleteIcons);
+      buttonDiv.append(editButtons);
+      buttonDiv.append(deleteButtons);
+      buttonCells.append(buttonDiv);
+      row.append(buttonCells);
+
+      $("#locationTableBody").append(row);
+    }
+  };
+
+  // -------- LOADING EMPLOYEE TABLE -------- //
   $.ajax({
     url: "libs/php/getAll.php",
     type: "POST",
     dataType: "json",
     success: function (result) {
       if (result.status.name == "ok") {
-        let allEmployeeData = result.data;
-        for (let i = 0; i < allEmployeeData.length; i++) {
-          // console.log(allEmployeeData[i]);
-          let row = $(`<tr id=employeeRow'${[i]}'>`).addClass(
-            "d-flex align-items-center"
-          );
-
-          let name_JobTableCell = $("<td>")
-            .addClass("ps-4 cell-width")
-            .attr("data-id", i);
-          let namesDiv = $("<div>").addClass(
-            "d-flex flex-column align-items-start"
-          );
-          let namesData = $("<p>")
-            .html(
-              `${allEmployeeData[i].firstName}, ${allEmployeeData[i].lastName}`
-            )
-            .addClass("fw-bold text-start mb-0");
-          let jobs = $("<p>").addClass("text-muted text-start mb-0");
-          let jobsData = $("<small>").html(allEmployeeData[i].jobTitle);
-
-          jobs.append(jobsData);
-          namesData.append(jobs);
-          namesDiv.append(namesData);
-          name_JobTableCell.append(namesDiv);
-          row.append(name_JobTableCell);
-
-          let emails = $("<td>")
-            .addClass("cell-width employeeEmail")
-            .attr("data-id", i);
-          emails
-            .html(allEmployeeData[i].email)
-            .addClass("text-nowrap align-middle d-md-table-cell ps-3");
-          row.append(emails);
-
-          let department_locationCell = $("<td>")
-            .addClass("cell-width pb-0 employeeDep")
-            .attr("data-id", i);
-          let departmentsDiv = $("<div>").addClass(
-            "d-flex flex-column align-items-start employeeDep"
-          );
-          let departmentsData = $("<p>")
-            .html(allEmployeeData[i].department)
-            .addClass("fw-bold text-start mb-0");
-          let locations = $("<p>").addClass("text-muted text-start mb-0");
-          let locationsData = $("<small>").html(allEmployeeData[i].location);
-
-          locations.append(locationsData);
-          departmentsData.append(locations);
-          departmentsDiv.append(departmentsData);
-          department_locationCell.append(departmentsDiv);
-          row.append(department_locationCell);
-
-          // -- BUTTONS -- //
-          let buttonCells = $("<td>").addClass("employeeCells");
-          let buttonDiv = $("<div>").addClass(
-            "d-flex justify-content-end employeeButtons me-3"
-          );
-          let editButtons = $("<button>")
-            .addClass("btn btn-none btn-sm")
-            .attr("data-bs-toggle", "modal")
-            .attr("data-bs-target", "#editEmployeeModal");
-          let editIcons = $("<i>").addClass("fa-solid fa-pencil fa-lg text-primary");
-          let deleteButtons = $("<button>")
-            .addClass("btn btn-none btn-sm ms-3")
-            .attr("data-bs-toggle", "modal")
-            .attr("data-bs-target", "#deleteEmployeeModal");
-          let deleteIcons = $("<i>").addClass("fa-regular fa-trash-can fa-lg text-danger");
-
-          editButtons.append(editIcons);
-          deleteButtons.append(deleteIcons);
-          buttonDiv.append(editButtons);
-          buttonDiv.append(deleteButtons);
-          buttonCells.append(buttonDiv);
-          row.append(buttonCells);
-
-          $("#employeeTableBody").append(row);
-        }
+        allEmployeeData = result.data;
+        fillTable(allEmployeeData);
       }
     },
     error: function (jqXHR, textStatus, errorThrown) {
@@ -108,6 +237,7 @@ $(document).ready(function () {
   // -------- LOADING DEPARTMENTS TABLE ----------- //
 
   let departments = [];
+  let allDepartmentData;
 
   $.ajax({
     url: "libs/php/getDepartments.php",
@@ -115,75 +245,8 @@ $(document).ready(function () {
     dataType: "json",
     success: function (result) {
       if (result.status.name == "ok") {
-        console.log(result);
-        let allDepartmentData = result.data;
-
-        for (let i = 0; i < allDepartmentData.length; i++) {
-          let department = allDepartmentData[i].department;
-          departments.push(department);
-
-          
-
-          let row = $(`<tr id=departmentRow'${[i]}'>`).addClass(
-            "d-flex"
-          );
-
-          // -- DEPARTMENT COLUMN -- //
-          let departmentDepartmentCells = $("<td>")
-            .addClass("ps-4 departmentDepartmentCells")
-            .attr("data-id", i);
-          let departmentDepartmentDiv = $("<div>").addClass(
-            "d-flex align-items-start"
-          );
-          let departmentDepartmentNames = $("<p>")
-            .html(department)
-            .addClass("fw-bold text-start mb-0");
-
-          departmentDepartmentDiv.append(departmentDepartmentNames);
-          departmentDepartmentCells.append(departmentDepartmentDiv);
-          row.append(departmentDepartmentCells);
-
-            // -- LOCATION COLUMN -- //
-          let departmentLocationCells = $("<td>")
-            .addClass("ps-4 departmentLocationCells")
-            .attr("data-id", i);
-          let departmentLocationDiv = $("<div>").addClass(
-            "d-flex align-items-start"
-          );
-          let departmentLocationNames = $("<p>")
-            .html(allDepartmentData[i].location)
-            .addClass("fw-bold text-start mb-0");
-
-          departmentLocationDiv.append(departmentLocationNames);
-          departmentLocationCells.append(departmentLocationDiv);
-          row.append(departmentLocationCells);
-
-          // -- BUTTONS -- //
-          let buttonCells = $("<td>").addClass("departmentButtonCells pb-4 pt-2");
-          let buttonDiv = $("<div>").addClass(
-            "d-flex justify-content-end employeeButtons me-3"
-          );
-          let editButtons = $("<button>")
-            .addClass("btn btn-none btn-sm")
-            .attr("data-bs-toggle", "modal")
-            .attr("data-bs-target", "#editDepartmentModal");
-          let editIcons = $("<i>").addClass("fa-solid fa-pencil fa-lg text-primary");
-          let deleteButtons = $("<button>")
-            .addClass("btn btn-none btn-sm ms-3")
-            .attr("data-bs-toggle", "modal")
-            .attr("data-bs-target", "#deleteDepartmentModal");
-          let deleteIcons = $("<i>").addClass("fa-regular fa-trash-can fa-lg text-danger");
-
-          editButtons.append(editIcons);
-          deleteButtons.append(deleteIcons);
-          buttonDiv.append(editButtons);
-          buttonDiv.append(deleteButtons);
-          buttonCells.append(buttonDiv);
-          row.append(buttonCells);
-
-          $("#departmentTableBody").append(row);
-        }
-        console.log(departments);
+        allDepartmentData = result.data;
+        fillDepartmentTable(allDepartmentData);
       }
     },
     error: function (jqXHR, textStatus, errorThrown) {
@@ -192,11 +255,10 @@ $(document).ready(function () {
     },
   });
 
+  // -------- LOADING LOCATIONS TABLE ----------- //
 
-
-    // -------- LOADING LOCATIONS TABLE ----------- //
-
-    let locations = [];
+  let locations = [];
+  let allLocationData;
 
   $.ajax({
     url: "libs/php/getLocations.php",
@@ -205,60 +267,8 @@ $(document).ready(function () {
     success: function (result) {
       if (result.status.name == "ok") {
         console.log(result);
-        let allLocationData = result.data;
-
-        for (let i = 0; i < allLocationData.length; i++) {
-          let location = allLocationData[i].location;
-          locations.push(location);
-
-          
-
-          let row = $(`<tr id=locationRow'${[i]}'>`).addClass(
-            "d-flex align-items-center"
-          );
-
-          // -- LOCATION COLUMN -- //
-          let locationLocationCells = $("<td>")
-            .addClass("ps-4 locationLocationCells")
-            .attr("data-id", i);
-          let locationLocationDiv = $("<div>").addClass(
-            "d-flex align-items-start"
-          );
-          let locationLocationNames = $("<p>")
-            .html(location)
-            .addClass("fw-bold text-start mb-0");
-
-          locationLocationDiv.append(locationLocationNames);
-          locationLocationCells.append(locationLocationDiv);
-          row.append(locationLocationCells);
-
-
-          // -- BUTTONS -- //
-          let buttonCells = $("<td>").addClass("locationButtonCells pb-4 pt-2");
-          let buttonDiv = $("<div>").addClass(
-            "d-flex justify-content-end employeeButtons me-3"
-          );
-          let editButtons = $("<button>")
-            .addClass("btn btn-none btn-sm")
-            .attr("data-bs-toggle", "modal")
-            .attr("data-bs-target", "#editDepartmentModal");
-          let editIcons = $("<i>").addClass("fa-solid fa-pencil fa-lg text-primary");
-          let deleteButtons = $("<button>")
-            .addClass("btn btn-none btn-sm ms-3")
-            .attr("data-bs-toggle", "modal")
-            .attr("data-bs-target", "#deleteDepartmentModal");
-          let deleteIcons = $("<i>").addClass("fa-regular fa-trash-can fa-lg text-danger");
-
-          editButtons.append(editIcons);
-          deleteButtons.append(deleteIcons);
-          buttonDiv.append(editButtons);
-          buttonDiv.append(deleteButtons);
-          buttonCells.append(buttonDiv);
-          row.append(buttonCells);
-
-          $("#locationTableBody").append(row);
-        }
-        console.log(locations);
+        allLocationData = result.data;
+        fillLocationTable(allLocationData);
       }
     },
     error: function (jqXHR, textStatus, errorThrown) {
@@ -267,13 +277,79 @@ $(document).ready(function () {
     },
   });
 
+  // ------------- GET SELECTED TAB ----------- //
 
+  let searchTerm;
+  let selectedTab = $(".active").attr("id");
+  if (selectedTab === "pills-employees-tab") {
+    $("#searchBar").on("keyup", function () {
+      searchTerm = $(this).val().toLowerCase(); // Get the current value of the input
+      console.log(allEmployeeData);
+      let filteredData = allEmployeeData.filter(function (val) {
+        if (
+          val.firstName.toLowerCase().startsWith(searchTerm) ||
+          val.lastName.toLowerCase().startsWith(searchTerm)
+        ) {
+          return val;
+        }
+      });
+      console.log(filteredData);
+      $("#employeeTableBody").empty();
 
+      fillTable(filteredData);
+    });
+  }
 
+  // ------------- FILTERING DATA FROM SEARCH BAR ----------- //
 
+  $(".nav-link").on("click", function () {
+    if ($(this).hasClass("active")) {
+      selectedTab = $(this).attr("id");
+      console.log(selectedTab);
+      if (selectedTab === "pills-employees-tab") {
+        $("#searchBar").on("keyup", function () {
+          searchTerm = $(this).val().toLowerCase(); // Get the current value of the input
+          let filteredData = allEmployeeData.filter(function (val) {
+            if (
+              val.firstName.toLowerCase().startsWith(searchTerm) ||
+              val.lastName.toLowerCase().startsWith(searchTerm)
+            ) {
+              return val;
+            }
+          });
+          $("#employeeTableBody").empty();
 
+          fillTable(filteredData);
+        });
+      } else if (selectedTab === "pills-departments-tab") {
+        $("#searchBar").on("keyup", function () {
+          searchTerm = $(this).val().toLowerCase();
+          let filteredData = allDepartmentData.filter(function (val) {
+            if (val.department.toLowerCase().startsWith(searchTerm)) {
+              return val;
+            }
+          });
+          $("#departmentTableBody").empty();
 
+          fillDepartmentTable(filteredData);
+        });
+      } else if (selectedTab === "pills-locations-tab") {
+        console.log("location");
 
+        $("#searchBar").on("keyup", function () {
+          searchTerm = $(this).val().toLowerCase();
+          let filteredData = allLocationData.filter(function (val) {
+            if (val.location.toLowerCase().startsWith(searchTerm)) {
+              return val;
+            }
+          });
+          $("#locationTableBody").empty();
+
+          fillLocationTable(filteredData);
+        });
+      }
+    }
+  });
 
 
 });
